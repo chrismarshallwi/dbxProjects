8969e0326e91b3bbd614fe270ed8c8801a29ede0
chrismarshallwi <chrismarshallwi>
2025-10-19 12:55:51 +0000

    adding the first option of RSI

---
+++ Alpha Factors/factors.py
@@ -0,0 +1,11 @@
+#%pip install streamlit
+
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+factors = ['Relative Strength Index (RSI)']
+
+st.selectbox('Alpha Factor',factors)
+
