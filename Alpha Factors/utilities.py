52b4a5c51c93f9c545d5162dc130eb9e9e8976cb
chrismarshallwi <chrismarshallwi>
2025-10-25 14:36:22 +0000

    finally got the class/method fixed and the table fact_price_daily written !

--- Alpha Factors/utilities.py
+++ Alpha Factors/utilities.py
@@ -6,10 +6,6 @@ class Market():
         self.tickers = tickers #what market are we creating?
         self.start = start #Start date 
         self.end = end #end date
-
-    # def get_price(self):
-    #     data = yf.download(tickers=self.ticker, start=self.start, end=self.end)
-    #     return data
     
     def get_price(self):
         data = yf.download(self.tickers, start=self.start, end=self.end, group_by='ticker')
