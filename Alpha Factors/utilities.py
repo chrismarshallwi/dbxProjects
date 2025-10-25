80cc244caf88b0512e0283625b7191e362ac18ec
chrismarshallwi <chrismarshallwi>
2025-10-25 14:11:26 +0000

    this method is not working. stripped down

--- Alpha Factors/utilities.py
+++ Alpha Factors/utilities.py
@@ -2,18 +2,11 @@ import pandas as pd
 import yfinance as yf
 
 class Market():
-    def __init__(self, tickers, start, end):
+    def __init__(self, ticker, start, end):
         self.tickers = tickers #what market are we creating?
         self.start = start #Start date 
         self.end = end #end date
 
     def get_price(self):
-        stock_data_df = []
-        for i in self.tickers:
-            data = yf.download(i, start=self.start, end=self.end)
-            #data['Symbol'] = i
-            #data['first_column'] = data.pop('Symbol')
-            #data.insert(0, 'Symbol', data['first_column'])
-            stock_data_df.append(data)
-        stock_data_df = pd.concat(stock_data_df)
-        return stock_data_df
+        data = yf.download(tickers=self.tickers, start=self.start, end=self.end)
+        return data
