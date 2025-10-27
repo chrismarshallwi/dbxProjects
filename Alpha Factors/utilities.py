392329c0b4dc76476adc4bc06260fd61a27b2401
chrismarshallwi <chrismarshallwi>
2025-10-27 13:30:48 +0000

    yaml file + config variable

--- Alpha Factors/utilities.py
+++ Alpha Factors/utilities.py
@@ -1,5 +1,18 @@
 import pandas as pd 
 import yfinance as yf
+import yaml
+
+def read_yaml_file(file_path):
+    '''
+    Access keys in yaml file
+    '''
+    with open(file_path, 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+
+yaml_path = 'connections.yaml'
+config_data = read_yaml_file(yaml_path)
+url_sp = config_data.get('spurl')
 
 class Market():
     def __init__(self, tickers, start, end):
@@ -17,3 +30,15 @@ class Market():
             all_data.append(df)
 
         return pd.concat(all_data)
+
+class Tickers():
+    def __init__(self):
+        pass
+
+    def get_tickers_sp():
+        '''
+        SP 500 Tickers
+        '''
+        ticker_list = []
+        tickers = pd.read_html(url_sp)[0].Symbol.to_list()
+        return tickers
