a794faeffc7a34662ed6d68e6ce45a69161afe90
chrismarshallwi <chrismarshallwi>
2025-11-10 13:28:56 +0000

    adding company main upstream

--- Finance/data/financial_statements/sql/fact_staging_financial_statement_tbl.sql
+++ Finance/data/financial_statements/sql/fact_staging_financial_statement_tbl.sql
@@ -1,22 +1,49 @@
-with sub as (
+with company_main as 
+(
+    with dim_cik as (select * from operations.finance_staging.raw_dim_cik) 
+    ,dim_exchange as (select * from operations.finance_staging.raw_dim_exchange)
+    ,dim_sp as (select * from operations.finance_staging.raw_dim_sp_500)
+
+    select 
+      sha2(concat_ws('|', dim_cik.cik, dim_cik.ticker), 256) AS company_key_hash
+      ,bigint(substr(xxhash64(concat_ws('|', dim_cik.cik, dim_cik.ticker)), 1, 18)) AS company_bigint_key
+      ,dim_cik.cik as company_identifier_key
+      ,dim_cik.ticker as company_stock_symbol
+      ,dim_cik.title as company_name
+      ,dim_exchange.exchange as exchange_listed_on
+      ,case when dim_sp.ticker is null then 0 else 1 end as sp_500_indicator
+      ,dim_sp.gics_sector as company_sector
+      ,dim_sp.gics_sub_industry as company_sub_industry
+      ,dim_sp.headquarters_location as company_headquarters_location
+      ,dim_sp.date_added as company_date_added_to_sp_500
+      ,dim_sp.Founded as company_year_founded
+    from 
+    dim_cik 
+    left join dim_exchange on dim_exchange.cik = dim_cik.cik and dim_exchange.ticker = dim_cik.ticker
+    left join dim_sp on dim_sp.ticker = dim_cik.ticker and dim_sp.CIK = dim_cik.cik 
+)
+
+,sub as (
 select 
 adsh as filing_key
 ,sub.cik as company_identifier
-,cik.ticker as ticker_symbol
-,exchange.exchange as exchange_traded_on
-,case when sp.CIK is null then 0 else 1 end as sp_500_indicator
+,dc.company_stock_symbol as ticker_symbol
+,dc.exchange_listed_on as exchange_traded_on
+,case when dc.company_identifier_key is null then 0 else 1 end as sp_500_indicator
 ,sub.name as name_of_filing_company
 ,form as name_of_submitted_form
 ,period as reported_period 
 ,fy as fiscal_year
 ,fp as fiscal_period
 ,filed as filing_date
-,sha2(concat_ws('|', cik.cik, cik.ticker), 256) AS company_key_hash
-,bigint(substr(xxhash64(concat_ws('|', cik.cik, cik.ticker)), 1, 18)) AS company_bigint_key
+/*,sha2(concat_ws('|', cik.cik, cik.ticker), 256) AS company_key_hash
+,bigint(substr(xxhash64(concat_ws('|', cik.cik, cik.ticker)), 1, 18)) AS company_bigint_key*/
+,dc.company_key_hash
+,dc.company_bigint_key
 from operations.finance_staging.raw_sub_tbl as sub
-left join (select * from operations.finance_staging.raw_dim_cik) as cik on cik.cik = sub.cik
+left join company_main dc on dc.company_identifier_key = sub.cik /*(select * from operations.finance_staging.raw_dim_cik) as cik on cik.cik = sub.cik
 left join (select * from operations.finance_staging.raw_dim_exchange) as exchange on exchange.cik = sub.cik
-left join (select * from operations.finance_staging.raw_dim_sp_500) as sp on sp.CIK = sub.cik
+left join (select * from operations.finance_staging.raw_dim_sp_500) as sp on sp.CIK = sub.cik*/
 
 ) 
 
