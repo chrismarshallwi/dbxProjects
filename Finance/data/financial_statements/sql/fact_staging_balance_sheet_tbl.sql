b4f006212a7493a16e988f2a330a73c97947204c
chrismarshallwi <chrismarshallwi>
2025-11-21 02:28:45 +0000

    solutions are not always as apparent as they may seem.
    Two dim tables. One schema table that should hopefully assert when companies dont record certain presented labels and a dim presented labels table that should connect the fact table to the schema table that asserts

--- Finance/data/financial_statements/sql/fact_staging_balance_sheet_tbl.sql
+++ Finance/data/financial_statements/sql/fact_staging_balance_sheet_tbl.sql
@@ -13,9 +13,10 @@ bs.company_bigint_key
 ,dc.company_stock_symbol
 ,bs.presented_label
 ,bs.terse_label  as terse_label
-,dt.tag_level_3 as terse_label_tag_level_3
+/*,dt.tag_level_3 as terse_label_tag_level_3*/
 ,dtx.tag_level_3 as presented_label_tag_level_3
-,coalesce(dt.tag_level_3,dtx.tag_level_3) as tag_level_3_combined
+/*,coalesce(dt.tag_level_3,dtx.tag_level_3) as tag_level_3_combined*/
+,dtx.tag_level_3 as tag_level_3_combined
 
 ,case when bs.presented_label like '%Total%' then 1 
     when bs.presented_label like '%TOTAL' then 1 
@@ -28,7 +29,7 @@ bs.company_bigint_key
 from 
 operations.finance_staging.fact_staging_financial_statement_tbl bs
 left join operations.finance.dim_company dc on dc.company_bigint_key = bs.company_bigint_key
-left join operations.finance.dim_tag dt on dt.tag_bigint_key = bs.tag_sub_total_bigint_key
+/*left join operations.finance.dim_tag dt on dt.tag_bigint_key = bs.tag_sub_total_bigint_key*/
 left join operations.finance.dim_tag dtx on dtx.tag_bigint_key = bs.tag_total_bigint_key
 where 
 financial_statement = 'BS'  and value_segment is null
@@ -47,15 +48,14 @@ dc.sp_500_indicator = 1)
             'Total Assets' as total_assets,
             'Total Liabilities' as total_liabilities,
             'Total Equity' as total_equity,
-            'Total Liabilities & Equity' as total_liabilities_and_equity
+            'Total Liabilities & Equity' as total_liabilities_and_equity,
+            'Total Current Assets' as total_current_assets,
+            case when 'Total Noncurrent Assets' is null then '1' else 'Total Noncurrent Assets' end as total_noncurrent_assets
         )
     )
 )
-select distinct * from pivot_cte 
+select distinct * from pivot_cte where company_stock_symbol = 'ADP' 
 
-/*
-select * from cte where company_stock_symbol = 'APA' order by report_number , report_line_number
-*/
 
 
 
