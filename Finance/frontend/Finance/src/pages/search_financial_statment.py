0286fee23c4ae35ac68cfbce9abebb2a4aee2681
chrismarshallwi <chrismarshallwi>
2026-01-06 01:56:27 +0000

    pivot logic

--- Finance/frontend/Finance/src/pages/search_financial_statment.py
+++ Finance/frontend/Finance/src/pages/search_financial_statment.py
@@ -6,17 +6,44 @@ catalog = 'operations'
 schema = 'finance_staging'
 table_name = 'fact_staging_financial_statement_tbl'
 
+# query = f"""
+# select distinct 
+# presented_label
+# ,report_line_number
+# ,reported_period
+# ,value
+# from 
+# {catalog}.{schema}.{table_name}
+# where ticker_symbol = 'DE'
+# and financial_statement = 'BS' and value_segment is null and reported_period = end_reported_period
+# order by report_line_number
+# """
+
+
 query = f"""
-select distinct 
-presented_label
-,report_line_number
-,reported_period
-,value
-from 
-{catalog}.{schema}.{table_name}
-where ticker_symbol = 'DE'
-and financial_statement = 'BS' and value_segment is null and reported_period = end_reported_period
-order by report_line_number
+SELECT *
+FROM (
+    SELECT
+        presented_label,
+        report_line_number,
+        reported_period,
+        value
+    FROM {catalog}.{schema}.{table_name}
+    WHERE ticker_symbol = 'DE'
+      AND financial_statement = 'BS'
+      AND value_segment IS NULL
+      AND reported_period = end_reported_period
+)
+PIVOT (
+    MAX(value)
+    FOR reported_period IN (
+        '20250430' AS `2025_04_30`,
+        '20250131' AS `2025-01-31`
+    )
+)
+ORDER BY report_line_number;
+
+
 """
 
 
