28469266a276fbcbe5056843e4f2e1ca9de124bc
chrismarshallwi <chrismarshallwi>
2026-02-16 13:31:00 +0000

    idk?

--- shared/notebooks/schema_manager/apply.py
+++ shared/notebooks/schema_manager/apply.py
@@ -2,4 +2,3 @@ from schema_manager.main import SchemaManager
 
 SchemaManager(dbutils.widgets.getAll()).run()
 
-C:\Users\cmarshal\OneDrive - Milwaukee Tool\Documents\databricks_dev\data-integration-finance\shared\notebooks\schema_manager\apply.ipynb
