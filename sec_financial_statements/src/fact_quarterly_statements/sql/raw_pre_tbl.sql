63b45a4a522359be16ec588dc16666a9ee18cb6f
chrismarshallwi <chrismarshallwi>
2026-04-09 00:08:57 +0000

    changing sql tables source

--- sec_financial_statements/src/fact_quarterly_statements/sql/raw_pre_tbl.sql
+++ sec_financial_statements/src/fact_quarterly_statements/sql/raw_pre_tbl.sql
@@ -2,56 +2,56 @@ INSERT OVERWRITE TABLE IDENTIFIER(:target_catalog || ".finance_staging.raw_pre_t
 select 
 *
 from (
-    select * from operations.finance_staging.raw_2025_q1_num_tbl
+    select * from operations.finance_staging.raw_2025_q1_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2025_q2_num_tbl
+    select * from operations.finance_staging.raw_2025_q2_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2025_q3_num_tbl
+    select * from operations.finance_staging.raw_2025_q3_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2025_q4_num_tbl
+    select * from operations.finance_staging.raw_2025_q4_pre_tbl
 /*2024*/
 union all 
-        select * from operations.finance_staging.raw_2024_q1_num_tbl
+        select * from operations.finance_staging.raw_2024_q1_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2024_q2_num_tbl
+    select * from operations.finance_staging.raw_2024_q2_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2024_q3_num_tbl
+    select * from operations.finance_staging.raw_2024_q3_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2024_q4_num_tbl
+    select * from operations.finance_staging.raw_2024_q4_pre_tbl
 /*2023*/
 union
-        select * from operations.finance_staging.raw_2023_q1_num_tbl
+        select * from operations.finance_staging.raw_2023_q1_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2023_q2_num_tbl
+    select * from operations.finance_staging.raw_2023_q2_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2023_q3_num_tbl
+    select * from operations.finance_staging.raw_2023_q3_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2023_q4_num_tbl
+    select * from operations.finance_staging.raw_2023_q4_pre_tbl
 /*2022*/
 union all
-        select * from operations.finance_staging.raw_2022_q1_num_tbl
+        select * from operations.finance_staging.raw_2022_q1_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2022_q2_num_tbl
+    select * from operations.finance_staging.raw_2022_q2_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2022_q3_num_tbl
+    select * from operations.finance_staging.raw_2022_q3_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2022_q4_num_tbl
+    select * from operations.finance_staging.raw_2022_q4_pre_tbl
 /*2021*/
 union all
-        select * from operations.finance_staging.raw_2021_q1_num_tbl
+        select * from operations.finance_staging.raw_2021_q1_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2021_q2_num_tbl
+    select * from operations.finance_staging.raw_2021_q2_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2021_q3_num_tbl
+    select * from operations.finance_staging.raw_2021_q3_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2021_q4_num_tbl
+    select * from operations.finance_staging.raw_2021_q4_pre_tbl
 /*2020*/
 union all
-        select * from operations.finance_staging.raw_2020_q1_num_tbl
+        select * from operations.finance_staging.raw_2020_q1_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2020_q2_num_tbl
+    select * from operations.finance_staging.raw_2020_q2_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2020_q3_num_tbl
+    select * from operations.finance_staging.raw_2020_q3_pre_tbl
     union all 
-    select * from operations.finance_staging.raw_2020_q4_num_tbl
+    select * from operations.finance_staging.raw_2020_q4_pre_tbl
   )
