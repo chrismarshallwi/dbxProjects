628986e51033cc2d428d70154a9e1849ce3c9334
chrismarshallwi <chrismarshallwi>
2026-04-10 17:45:05 +0000

    progress was made. Doing a PR

--- sec_financial_statements/src/fact_balance_sheet/sql/fact_balance_sheet_version_one.sql
+++ sec_financial_statements/src/fact_balance_sheet/sql/fact_balance_sheet_version_one.sql
@@ -1,11 +1,10 @@
-insert overwrite IDENTIFIER(:target_catalog || '.finance_staging.fact_balance_sheet_version_one')
+--insert overwrite IDENTIFIER(:target_catalog || '.finance_staging.fact_balance_sheet_version_one')
 WITH cte AS (
     SELECT DISTINCT
          dc.company_bigint_key
         ,reported_period as date_key
         ,value
         ,dt.terse_label
-
         ,CASE
             WHEN fa.terse_label = 'Assets'
                  AND dt.terse_label_level_3 = 'Assets [Abstract]'
@@ -75,6 +74,32 @@ WITH cte AS (
             WHEN fa.terse_label = 'Limited Partners'' Capital Account'
                  AND dt.linkrole = 'http://fasb.org/us-gaap/role/statement/StatementOfFinancialPositionClassified-RealEstateOperations'
                  AND fa.value_segment IS NULL
+            THEN 'Total Equity'
+
+            -- NEW: Stockholders' Equity taxonomy naming (prevalent in 2022/2023 data)
+
+            WHEN fa.terse_label = "Stockholders' Equity, Including Portion Attributable to Noncontrolling Interest"
+                 AND dt.terse_label_level_4 = "Stockholders' Equity, Including Portion Attributable to Noncontrolling Interest [Abstract]"
+                 AND dt.terse_label_level_5 = "Stockholders' Equity, Including Portion Attributable to Noncontrolling Interest"
+                 AND fa.value_segment IS NULL
+            THEN 'Total Equity'
+
+            WHEN fa.terse_label = "Stockholders' Equity Attributable to Parent"
+                 AND dt.terse_label_level_4 = "Stockholders' Equity, Including Portion Attributable to Noncontrolling Interest [Abstract]"
+                 AND dt.terse_label_level_5 = "Stockholders' Equity Attributable to Parent [Abstract]"
+                 AND dt.terse_label_level_6 = "Stockholders' Equity Attributable to Parent"
+                 AND fa.value_segment IS NULL
+                 AND NOT EXISTS (
+                     SELECT 1
+                     FROM operations.finance_staging.fact_staging_financial_statement fa2
+                     JOIN operations.finance.dim_taxonomy dt2
+                         ON dt2.terse_label = fa2.terse_label
+                         AND dt2.gaap_version = fa2.gaap_version
+                         AND dt2.linkrole = dt.linkrole
+                     WHERE fa2.filing_key = fa.filing_key
+                       AND fa2.terse_label = "Stockholders' Equity, Including Portion Attributable to Noncontrolling Interest"
+                       AND fa2.value_segment IS NULL
+                 )
             THEN 'Total Equity'
 
             WHEN fa.terse_label = 'Liabilities and Equity'
@@ -184,11 +209,15 @@ WITH cte AS (
       AND name_of_submitted_form = '10-Q'
       AND financial_statement = 'BS'
       AND value_segment IS NULL
+      --and dc.company_stock_symbol = "ABT"
 )
+
+
+
 
 SELECT
      cte.company_bigint_key
-
+,dc.company_name
     ,date_key
 
     -- Top level
@@ -291,11 +320,11 @@ LEFT JOIN operations.finance.dim_company dc
     ON dc.company_bigint_key = cte.company_bigint_key
 WHERE bs_component IS NOT NULL
 GROUP BY
-     cte.company_bigint_key
+     cte.company_bigint_key,dc.company_name
     ,date_key
 
 ORDER BY
-     cte.company_bigint_key
+     cte.company_bigint_key,dc.company_name
     ,date_key
 
 
