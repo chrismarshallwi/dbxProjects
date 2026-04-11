ea5eb8cd972bb0dec1137a3127cc1a58beba683e
chrismarshallwi <chrismarshallwi>
2026-04-11 15:15:56 +0000

    ordering plus removing stale code

--- Finance/frontend/Finance/src/pages/search_balance_sheet.py
+++ Finance/frontend/Finance/src/pages/search_balance_sheet.py
@@ -17,45 +17,17 @@ class SearchBalanceSheet:
 
         ticker_filter = ", ".join(f"'{t}'" for t in selected_tickers)
 
-        #dont think that sql pivot will be suffice here
-        #consider using python in future
-
-        # query = f"""
-        # SELECT *
-        # FROM (
-        #     SELECT
-        #         presented_label,
-        #         report_line_number,
-        #         reported_period,
-        #         value
-        #     FROM {catalog}.{schema}.{table_name}
-        #     WHERE ticker_symbol in ({ticker_filter})
-        #     AND financial_statement = 'BS'
-        #     AND value_segment IS NULL
-        #     AND reported_period = end_reported_period
-        # )
-        # PIVOT (
-        #     MAX(value)
-        #     FOR reported_period IN (
-        #         '20241231' AS `2024_12_31`,
-        #         '20250331' AS `2025-03-31`
-
-        #     )
-        # )
-        # ORDER BY report_line_number;
-        # """
-
         reported_periods_query = f"""
         SELECT DISTINCT reported_period
-FROM operations.finance_staging.fact_staging_financial_statement fa
-LEFT JOIN operations.finance.dim_company dc 
-    ON dc.company_bigint_key = fa.company_bigint_key 
-WHERE dc.company_stock_symbol in ({ticker_filter})
-    AND financial_statement = 'BS'
-    AND reported_period = end_reported_period
-    AND value_segment IS NULL
-    AND name_of_submitted_form = '10-Q'
-ORDER BY reported_period
+        FROM operations.finance_staging.fact_staging_financial_statement fa
+        LEFT JOIN operations.finance.dim_company dc 
+        ON dc.company_bigint_key = fa.company_bigint_key 
+        WHERE dc.company_stock_symbol in ({ticker_filter})
+        AND financial_statement = 'BS'
+        AND reported_period = end_reported_period
+        AND value_segment IS NULL
+        AND name_of_submitted_form = '10-Q'
+        ORDER BY reported_period
         """
 
         periods_df = sql_query(sql_query = reported_periods_query)
@@ -71,9 +43,12 @@ ORDER BY reported_period
                 terse_label,
                 reported_period,
                 value
-            from operations.finance_staging.fact_staging_financial_statement fa
-            left join operations.finance.dim_company dc 
-                on dc.company_bigint_key = fa.company_bigint_key 
+                ,report_number
+                ,report_line_number
+            from 
+            operations.finance_staging.fact_staging_financial_statement fa
+            left join 
+            operations.finance.dim_company dc  on dc.company_bigint_key = fa.company_bigint_key 
             WHERE dc.company_stock_symbol in ({ticker_filter})
                 and financial_statement = 'BS'
                 and reported_period = end_reported_period
@@ -84,10 +59,8 @@ ORDER BY reported_period
             MAX(value)
             FOR reported_period IN ({pivot_list})
         )
-        ORDER BY terse_label
+        ORDER BY report_number, report_line_number
         """
-
-        #spark.sql(query)
 
         df = sql_query(sql_query=query)
         st.data_editor(df, use_container_width=True, hide_index=True)
