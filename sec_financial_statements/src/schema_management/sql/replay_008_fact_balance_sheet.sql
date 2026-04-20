50050f5ce1566160cac136495ab1e6fc092af199
chrismarshallwi <chrismarshallwi>
2026-04-20 18:21:24 +0000

    submitted form fix.

--- sec_financial_statements/src/schema_management/sql/replay_008_fact_balance_sheet.sql
+++ sec_financial_statements/src/schema_management/sql/replay_008_fact_balance_sheet.sql
@@ -3,6 +3,7 @@ CREATE or replace TABLE operations.finance.fact_balance_sheet (
   company_bigint_key BIGINT,
   date_key BIGINT,
   capital_structure_business_key integer COMMENT "Signifies if financial companies, real estate companies as having different structured balance sheet versus regular companies with normal balance sheet",
+  submitted_form_business_key integer comment "Signifies what form was submitted. Typically, 10-Q's are filed ever quarter and 10-K's are filed annually. Some (Most) companies file 3 10-Q's and 1 10-K per year.",
   total_assets DOUBLE,
   total_liabilities DOUBLE,
   total_equity DOUBLE,
