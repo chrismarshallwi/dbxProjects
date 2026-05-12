8672a121ae6aaec124327156177c6af944556a50
chrismarshallwi <chrismarshallwi>
2026-05-12 19:54:32 +0000

    first pass at creating the fact income statement table - total revenue only.

--- sec_financial_statements/src/fact_income_statement/sql/fact_income_statement.sql
+++ sec_financial_statements/src/fact_income_statement/sql/fact_income_statement.sql
@@ -1,50 +1,145 @@
+/*Where I last left off:
+Discovered that interesting for one company (Fortinet) that they report in 2023 GROSS MARGIN as a terse label. This terse label does not show up in the dim taxonomy table. The standard label, is Gross Profit which does correspond to a child label in the Dim taxonomy table. When joining the dim taxonomy table, try joining on Standard Lable = Child and you will have to remove some characters from Child like us-gaap: to get the standard label match. 
+*/
+
+insert overwrite IDENTIFIER(:target_catalog || '.finance.fact_income_statement')
+
 with base as (
 select distinct 
-dc.company_bigint_key
-,dcc.company_name 
-,dcc.company_stock_symbol
+dc.company_name
+,fa.company_bigint_key
 ,fa.reported_period 
 ,fa.end_reported_period
+,fa.value_segment
 ,fa.value
-,dt.terse_label
-,fa.name_of_submitted_form as name_of_submitted_form
-,fa.fiscal_period as fiscal_period
-,fa.fiscal_year as fiscal_year
+,fa.terse_label
+,fa.standard_label
+,fa.name_of_submitted_form
+,fa.fiscal_period
+,fa.fiscal_year
 ,fa.reported_quarters
+,ds.preferred_fasb_linkrole_income_statement 
+,dense_rank() over (partition by dt.terse_label order by dt.terse_label_level_3) as duplicate_roll_up_ranking --denotes whether a terse label rolls up into 2 different categories. 
+,coalesce(dt.linkrole, dt2.linkrole) as linkrole 
+,coalesce(dt.terse_label_level_1 , dt2.terse_label_level_1  ) as terse_label_level_1 
+,coalesce(dt.terse_label_level_2 , dt2.terse_label_level_2  ) as terse_label_level_2
+,coalesce(dt.terse_label_level_3  , dt2.terse_label_level_3  ) as terse_label_level_3 
+,coalesce(dt.terse_label_level_4  , dt2.terse_label_level_4  ) as terse_label_level_4 
+,coalesce(dt.terse_label_level_5  , dt2.terse_label_level_5  ) as terse_label_level_5 
+,coalesce(dt.terse_label_level_6  , dt2.terse_label_level_6  ) as terse_label_level_6 
+,coalesce(dt.terse_label_level_7 , dt2.terse_label_level_7  ) as terse_label_level_7
+,coalesce(dt.terse_label_level_8  , dt2.terse_label_level_8  ) as terse_label_level_8 
+,coalesce(dt.terse_label_level_9 , dt2.terse_label_level_9  ) as terse_label_level_9 
+,coalesce(dt.terse_label_level_10  , dt2.terse_label_level_10  ) as terse_label_level_10 
+,coalesce(dt.linkrole, dt2.linkrole) as linkrole
+,coalesce(dt.terse_label_level_11 , dt2.terse_label_level_11  ) as terse_label_level_11 
+,coalesce(dt.terse_label_level_12 , dt2.terse_label_level_12  ) as terse_label_level_12
+,coalesce(dt.terse_label_level_13  , dt2.terse_label_level_13  ) as terse_label_level_13 
+,coalesce(dt.terse_label_level_14  , dt2.terse_label_level_14  ) as terse_label_level_14 
+,coalesce(dt.terse_label_level_15  , dt2.terse_label_level_15  ) as terse_label_level_15 
+,coalesce(dt.terse_label_level_16  , dt2.terse_label_level_16  ) as terse_label_level_16 
+,coalesce(dt.terse_label_level_17 , dt2.terse_label_level_17  ) as terse_label_level_17
+,coalesce(dt.terse_label_level_18  , dt2.terse_label_level_18  ) as terse_label_level_18 
+,coalesce(dt.terse_label_level_19 , dt2.terse_label_level_19  ) as terse_label_level_19 
+
 
 from 
 operations.finance_staging.fact_staging_financial_statement fa
-LEFT JOIN 
-     operations.finance.dim_taxonomy dt ON dt.terse_label = fa.terse_label AND dt.gaap_version = fa.gaap_version
-    left join 
-     operations.finance.dim_sector dc on 
-    (
-      dc.company_bigint_key = fa.company_bigint_key 
-      and 
-      dc.date_key = fa.reported_period
-      and 
-      dc.preferred_fasb_linkrole_income_statement = dt.linkrole
-    )
+left join 
+operations.finance.dim_company dc on dc.company_bigint_key = fa.company_bigint_key
+left join 
+operations.finance.dim_sector ds on 
+(
+    ds.company_bigint_key = fa.company_bigint_key
+    and 
+    ds.date_key = fa.reported_period
+)
+left join 
+operations.finance.dim_taxonomy dt on 
+(
+    fa.terse_label = dt.terse_label
+    and  
+    fa.gaap_version = dt.gaap_version 
+    and 
+    dt.linkrole = ds.preferred_fasb_linkrole_income_statement
+)
 
-left join operations.finance.dim_company dcc on dcc.company_bigint_key = fa.company_bigint_key
+left join 
+(select * from operations.finance.dim_taxonomy where linkrole like '%disclosure%') dt2 on 
+(
+    fa.terse_label = dt2.terse_label 
+    and 
+    fa.gaap_version = dt2.gaap_version 
+)
 
 where financial_statement = 'IS'
 and reported_period = end_reported_period
 and value_segment is null
 and name_of_submitted_form in ('10-Q','10-K')
 and reported_quarters in (1,4)
-and dc.preferred_fasb_linkrole_income_statement is not null
-and fa.company_bigint_key = 436988458547446346
 
+) 
+
+/*Input Logic below that condenses terse label conditions corresponding to highest level leaf node to compose the ins_component (income statement component) field*/
+,staging as (
+select distinct 
+* 
+,case when terse_label = 'Revenue from Contract with Customer, Excluding Assessed Tax' and terse_label_level_6 = 'Revenue from Contract with Customer, Excluding Assessed Tax' then 'Revenue'
+when terse_label = 'Revenues' and terse_label_level_12 = 'Revenues' then 'Revenue'
+else null 
+end as ins_component
+from base 
+where company_bigint_key is not null
+and duplicate_roll_up_ranking = 1
 )
 
+
+
 select 
-* 
-/*,case when reported_quarters = 4 then sum(value) over (partition by company_bigint_key,company_stock_symbol,fiscal_year, fiscal_period ,name_of_submitted_form,terse_label) else null end as total_fiscal_year
-,case when reported_quarters = 1 then sum(value) over (partition by company_bigint_key,company_stock_symbol,fiscal_year ,name_of_submitted_form,terse_label) else null end as total_fiscal_year_qtrs
-,case when reported_quarters = 4 then null else value end as value_x*/
+company_bigint_key
+,reported_period as date_key_reported_period
+
+/*,fiscal_period
+,fiscal_year*/
+
+     ,cast(concat(fiscal_year, case when (case when fiscal_period = 'Q1' then 1 
+          when fiscal_period = 'Q2' then 2 
+          when fiscal_period = 'Q3' then 3
+          when fiscal_period = 'FY' then 4 
+          end ) = 1 then '03' 
+     when (case when fiscal_period = 'Q1' then 1 
+          when fiscal_period = 'Q2' then 2 
+          when fiscal_period = 'Q3' then 3
+          when fiscal_period = 'FY' then 4 
+          end ) = 2 then '06'
+     when (case when fiscal_period = 'Q1' then 1 
+          when fiscal_period = 'Q2' then 2 
+          when fiscal_period = 'Q3' then 3
+          when fiscal_period = 'FY' then 4 
+          end ) = 3 then '09'
+     when (case when fiscal_period = 'Q1' then 1 
+          when fiscal_period = 'Q2' then 2 
+          when fiscal_period = 'Q3' then 3
+          when fiscal_period = 'FY' then 4 
+          end ) = 4 then '12'
+          end, '01' ) as integer) as date_key_converted_period
+
+,case when name_of_submitted_form = '10-Q' then 0 when name_of_submitted_form = '10-K' then 1 else null end as submitted_form_business_key 
+
+,reported_quarters
+--,ins_component
+--,value 
+
+,MAX(CASE WHEN ins_component = 'Revenue' THEN value END) AS total_revenue
 
 from 
-base 
+staging 
+where ins_component is not null 
+group by 
 
-
+company_bigint_key
+,reported_period 
+,name_of_submitted_form
+,fiscal_period
+,fiscal_year
+,reported_quarters
