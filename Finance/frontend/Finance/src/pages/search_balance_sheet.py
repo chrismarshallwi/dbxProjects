7879ed7566fab1a55aa8072ddbf70bb7089ae35a
chrismarshallwi <chrismarshallwi>
2026-06-17 19:01:42 +0000

    comments

--- Finance/frontend/Finance/src/pages/search_balance_sheet.py
+++ Finance/frontend/Finance/src/pages/search_balance_sheet.py
@@ -5,8 +5,8 @@ class SearchBalanceSheet:
     def __init__(self):
         
         catalog = 'operations'
-        schema = 'finance_staging'
-        table_name = 'fact_staging_financial_statement_tbl'
+        schema = 'finance'
+        table_name = 'fact_balance_sheet'
 
         tickers = get_tickers()
         selected_tickers = st.multiselect("Select Tickers",options=tickers,default=None)
@@ -17,24 +17,24 @@ class SearchBalanceSheet:
 
         ticker_filter = ", ".join(f"'{t}'" for t in selected_tickers)
 
-        reported_periods_query = f"""
-        SELECT DISTINCT reported_period
-        FROM operations.finance_staging.fact_staging_financial_statement fa
-        LEFT JOIN operations.finance.dim_company dc 
-        ON dc.company_bigint_key = fa.company_bigint_key 
-        WHERE dc.company_stock_symbol in ({ticker_filter})
-        AND financial_statement = 'BS'
-        AND reported_period = end_reported_period
-        AND value_segment IS NULL
-        AND name_of_submitted_form = '10-Q'
-        ORDER BY reported_period
-        """
+        # reported_periods_query = f"""
+        # SELECT DISTINCT reported_period
+        # FROM operations.finance_staging.fact_staging_financial_statement fa
+        # LEFT JOIN operations.finance.dim_company dc 
+        # ON dc.company_bigint_key = fa.company_bigint_key 
+        # WHERE dc.company_stock_symbol in ({ticker_filter})
+        # AND financial_statement = 'BS'
+        # AND reported_period = end_reported_period
+        # AND value_segment IS NULL
+        # AND name_of_submitted_form = '10-Q'
+        # ORDER BY reported_period
+        # """
 
-        periods_df = sql_query(sql_query = reported_periods_query)
+        # periods_df = sql_query(sql_query = reported_periods_query)
 
-        periods = periods_df['reported_period'].tolist()
+        # periods = periods_df['reported_period'].tolist()
 
-        pivot_list = ", ".join([f"'{p}'" for p in periods])
+        # pivot_list = ", ".join([f"'{p}'" for p in periods])
 
         query = f"""
  
