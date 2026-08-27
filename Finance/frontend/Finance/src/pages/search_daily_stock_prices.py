6bddcf6476ccc30cda9edc2acc8bb55ea7e4e6e3
Christopher Marshall <chrismarshall.wi@icloud.com>
2026-08-27 13:41:39 -0500

    order of operations - will ticker filter work for dates filter

--- Finance/frontend/Finance/src/pages/search_daily_stock_prices.py
+++ Finance/frontend/Finance/src/pages/search_daily_stock_prices.py
@@ -8,6 +8,7 @@ class SearchDailyStockPrice:
         schema = 'finance'
         table_name = 'fact_price_daily'
         table_name_dc = 'dim_company'
+        table_name_dd = 'dim_date'
 
         tickers = get_tickers()
         selected_tickers = st.multiselect("Select Tickers",options=tickers,default=None)
@@ -18,20 +19,44 @@ class SearchDailyStockPrice:
 
         ticker_filter = ", ".join(f"'{t}'" for t in selected_tickers)
 
-        #dont think that sql pivot will be suffice here
-        #consider using python in future
+        min_value_on_dataset = spark.sql(f"""
+        select 
+        min(dd.date_value) as min_date
+        from 
+        {catalog}.{schema}.{table_name} fa 
+        left join {catalog}.{schema}.{table_name_dd} dd on dd.date_key = fa.date_key 
+        left join {catalog}.{schema}.{table_name_dc} dc on dc.company_bigint_key = fa.company_bigint_key
+        where dc.company_stock_symbol in ({ticker_filter}) """).first()['min_date']
+
+        max_value_on_dataset = spark.sql(f"""
+        select 
+        max(dd.date_value) as max_value
+        from 
+        {catalog}.{schema}.{table_name} fa 
+        left join {catalog}.{schema}.{table_name_dd} dd on dd.date_key = fa.date_key 
+        left join {catalog}.{schema}.{table_name_dc} dc on dc.company_bigint_key = fa.company_bigint_key
+        where dc.company_stock_symbol in ({ticker_filter}) """).first()['max_date']
+
+        date_range = st.date_input(
+            'Select Date Range', 
+            value=None, 
+            min_value=min_value_on_dataset, 
+            max_value=max_value_on_dataset
+            )
 
         query = f"""
         select 
-        date_key,
-        adj_close
+        dd.date_value,
+        fa.adj_close
         from 
         {catalog}.{schema}.{table_name} fa
         left join 
         {catalog}.{schema}.{table_name_dc} dc on dc.company_bigint_key = fa.company_bigint_key
+        left join 
+        {catalog}.{schema}.{table_name_dd} dd on dd.date_key = fa.date_key
         where dc.company_stock_symbol in ({ticker_filter})
         """
 
-
         df = sql_query(sql_query=query)
-        st.data_editor(df, use_container_width=True, hide_index=True)
+        st.line_chart(df, x= 'date_value', y= 'adj_close')
+        #st.data_editor(df, use_container_width=True, hide_index=True)
